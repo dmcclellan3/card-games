d556b4848854798b02fe5a5a4b4bf12ce63bc2dd
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-08 13:35:16 -0400

    adding to gitignore

--- card-games.py
+++ card-games.py
@@ -1,14 +1,24 @@
-import cards
-import random
-
 DeckValue = list(range(1, 53))
-Suites = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+Suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
 CardValues = list(range(2,15))
 FaceValueCards = {11: Jack, 12: Queen, 13: King, 14: Ace}
 Spades = list(range(2,15))
 Hearts = list(range(2,15))
 Diamonds = list(range(2,15))
 Clubs = list(range(2,15))
+
+##Deck Class
+##init 
+##how many decks are there?
+
+
+##Random card function
+
+##shuffle
+
+##Card class
+
+
 
 
 
