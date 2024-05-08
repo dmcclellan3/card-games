ba6ba0580e17955b6a9458913c1635360650c17f
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-08 11:52:46 -0400

    initial save

---
+++ card-games.py
@@ -0,0 +1,19 @@
+import cards
+import random
+
+DeckValue = list(range(1, 53))
+Suites = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+CardValues = list(range(2,15))
+FaceValueCards = {11: Jack, 12: Queen, 13: King, 14: Ace}
+Spades = list(range(2,15))
+Hearts = list(range(2,15))
+Diamonds = list(range(2,15))
+Clubs = list(range(2,15))
+
+
+
+
+
+
+
+
