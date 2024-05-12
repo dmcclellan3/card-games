9f39010f3bd46a71bd5710369a62bbc6ffe5dd0e
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-12 15:09:30 -0400

    saving

--- card-games.py
+++ card-games.py
@@ -2,45 +2,17 @@ import random
 
 Deck_Value = list(range(1, 52))
 suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
-suits_values = []
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 number_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-# Card_Hand = ['1']
-# Score = [0]
-Face_Value_Cards = {'J': 11, 
-                    'Q': 12, 
-                    'K': 13,  
-                    'A': 14,}
 
-##Todo list: 
-## number of decks needs to ask for user input 
-##How does one suit beat another or tie? 
-
-
-
-
-# class Game:
-#     def __init__(self, PlayerTurn, CardHand, WinningHand, Score, Suits, Deck_Value, players):
-#         self.playerturn = PlayerTurn
-#         self.cardhand = CardHand
-#         self.score = Score
-#         self.winninghand = WinningHand
-#         self.suits = Suits['Spades', 'Hearts', 'Clubs', 'Diamonds']
-#         self.deckvalue = Deck_Value
-#         self.players = players  
 
 
         
 class Player:
-    def __init__(self, name_param, score=0):
+    def __init__(self, name_param):
         self.name = name_param
         self.card = None
-        self.score = score
-
-    def increment_score(self):
-        self.score += 1
-        print("Player 1 Score: ", player1.score)
-        print("Dealer Score: ", dealer.score)
+        
 
 player1 = Player('Duke')
 print('Player 1: ', player1.name)
@@ -61,18 +33,11 @@ class StartGame:
         if player1.card.numbervalue > dealer.card.numbervalue:
             print('Player 1 Wins!')
             return False
-            # player1.increment_score()
         elif dealer.card.numbervalue > player1.card.numbervalue:
             print('Dealer Wins!')
             return False 
-            # print('Dealer Score :')
-            # dealer.increment_score()
+            
         else: return True 
-        
-        # self.check_winner()
-        
-        ##player1.card.numbervalue == dealer.card.numbervalue:
-            ##print("It's a tie!")
 
 new_game = StartGame(1) 
 
@@ -87,16 +52,12 @@ class Card:
         
 def generate_cards(values, suits):
     cards = []
-    for index, value in enumerate(values):
+    for index, value in enumerate(values):  ## yields pairs 
         for suit in suits:                  ##This section takes cards values and suits from the array 
             cards.append(Card(value, suit, NumberValue=number_values[index])) ##and appends the card object to the value and suit
     random.shuffle(cards)                   ##Then we can use the random function that was imported and shuffle the cards list 
     return cards
-            # if value in Face_Value_Cards:   
-            #     face_card = Face_Value_Cards[value]
-            #     # cards.append((face_card, suit))
-            # else:
-            #     cards.append((value, suit))
+            
 
 class Deck:
     def __init__(self, Suits, CardNumbers, DeckValue, CardCount):
@@ -110,8 +71,6 @@ class Deck:
     def deal(self):
         player1.card = self.cards.pop()
         dealer.card = self.cards.pop()
-        # self.cards = Cards
-        # temp_list_of_cards = generate_cards(values, suits)
         
 current_game_deck = Deck(suits, values, Deck_Value, len(Deck_Value)) 
 
